fa87d325cea56b189fac442709cca410e586790b
Logan Riffell <lkriffell@gmail.com>
2021-02-26 16:24:02 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
+require 'faker'
+
+main_user = FactoryBot.create(:user)
+follower1 = FactoryBot.create(:user)
+follower2 = FactoryBot.create(:user)
+follower3 = FactoryBot.create(:user)
+follower4 = FactoryBot.create(:user)
+
+main_user.followers.create(follower_id: follower1.id)
+main_user.followers.create(follower_id: follower2.id)
+main_user.followers.create(follower_id: follower3.id)
+main_user.followers.create(follower_id: follower4.id)
+
+beat1 = main_user.beats.create(name:'Mac Miller Type Beat')
+beat2 = main_user.beats.create(name:'Hard Trap Beat')
+
+beat1.beat_tags.create(tag: 'chill')
+beat1.beat_tags.create(tag: 'boom bap')
+
+beat2.beat_tags.create(tag: 'hard')
+beat2.beat_tags.create(tag: 'trap')
