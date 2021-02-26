b012a583299f6f69e704e42d4c5de689c5d29354
Logan Riffell <lkriffell@gmail.com>
2021-02-26 16:23:05 -0700

    Add image validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates :username, uniqueness: true, presence: true
+  validates :image, presence: true
   validates :password, presence: true
   has_many :beats
   has_many :followers, foreign_key: 'user_id', class_name: 'UserFollow', dependent: :destroy
