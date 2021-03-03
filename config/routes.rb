4cfd6c815942f947d83a69f80206c2123718c65f
Logan Riffell <lkriffell@gmail.com>
2021-03-03 17:30:47 -0500

    Add route for samples index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
 
+  # Samples
+  get '/samples', to: 'samples#index'
+
+  # Users
   get '/users', to: 'users#index'
   get '/users/new', to: 'users#new'
   post '/users/new', to: 'users#create'
