069d7028b80f462d7886f8bcf4ec8852e7dc4325
Logan Riffell <lkriffell@gmail.com>
2021-03-03 11:07:32 -0500

    Add happy/sad testing for user show

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,28 +3,23 @@ require 'rails_helper'
 RSpec.describe 'user show' do
   describe 'happy paths' do
     it 'can be shown' do
-      visit '/users/1'
+      user = User.first
+      visit "/users/#{user.id}"
 
       expect(page).to have_content("Billy Joel")
       expect(page).to have_content("Followers: 4")
       expect(page).to have_content("Following: 0")
       expect(page).to have_content("Beats")
+      expect(page).to have_content(user.beats.first.name)
+      expect(page).to have_content(user.beats.last.name)
     end
   end
   describe 'sad paths' do
-    # it 'cannot be registered without username' do
-    #   visit users_new_path
-      
-    #   fill_in 'user_email', with: 'someone@email.com'
-    #   fill_in 'user_password', with: '1234'
-    #   fill_in 'user_image', with: 'https://www.fillmurray.com/200/200'
+    it 'cannot be registered without username' do
+      visit "/users/12345"
 
-    #   click_button('Save User')
-
-    #   expect(current_path).to eq("/users/new")
-
-    #   expect(page).to have_content("Something went wrong, did you fill out all fields?")
-    # end
+      expect(page).to have_content("It appears the user you're looking for does not exist")
+    end
 
     # it 'cannot be registered without password' do
     #   visit users_new_path
