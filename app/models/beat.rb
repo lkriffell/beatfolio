bd626a7423414db34b5793577c984604f18938e4
Logan Riffell <lkriffell@gmail.com>
2021-03-03 11:25:44 -0500

    Add dependent: :destroy

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -2,6 +2,6 @@ class Beat < ApplicationRecord
   validates :user_id, presence: true
   validates :name, presence: true
 
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :beat_tags
 end
--- app/models/user_follow.rb
+++ app/models/user_follow.rb
@@ -2,5 +2,5 @@ class UserFollow < ApplicationRecord
   validates :user_id, presence: true
   validates :follower_id, presence: true
 
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
 end
