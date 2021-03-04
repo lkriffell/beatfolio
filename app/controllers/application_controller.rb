92a22a480d5ba5ede8bdc1f33b1bfdbb1a45ff16
Logan Riffell <lkriffell@gmail.com>
2021-03-04 12:22:49 -0500

    Add helper methods for logged_in_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :logged_in_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def logged_in_user
+    unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+    end
   end
 end
