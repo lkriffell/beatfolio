487c097d6b4eb16a60657c94e7bdb07fb1121d67
Logan Riffell <lkriffell@gmail.com>
2021-03-07 13:16:58 -0500

    Refactor sample poro

--- app/poros/sample.rb
+++ app/poros/sample.rb
@@ -1,11 +1,12 @@
 class Sample
-  attr_accessor :title, :key, :download_count, :url, :creator, :bpm
+  attr_accessor :title, :key, :download_count, :url, :audio_url, :creator, :bpm
 
-  def initialize(title, key, download_count, url, creator, bpm)
+  def initialize(title, key, download_count, url, audio_url, creator, bpm)
     @title = title
     @key = key
     @download_count = download_count
     @url = url
+    @audio_url = audio_url
     @creator = creator
     @bpm = bpm
   end
