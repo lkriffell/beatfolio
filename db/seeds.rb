2ddad2db676638a8c2a4efea7d4b2ef0e70b19d9
Logan Riffell <lkriffell@gmail.com>
2021-03-13 11:50:09 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,11 @@ require 'factory_bot_rails'
 require 'faker'
 
 User.delete_all
-main_user = FactoryBot.create(:user, username: 'Billy Joel', email: 'BillJ@gmail.com')
+Beat.delete_all
+BeatTag.delete_all
+UserFollow.delete_all
+
+main_user = FactoryBot.create(:user, username: 'lkriffell', email: 'lkriffell@gmail.com')
 follower1 = FactoryBot.create(:user)
 follower2 = FactoryBot.create(:user)
 follower3 = FactoryBot.create(:user)
@@ -13,11 +17,15 @@ main_user.followers.create(follower_id: follower2.id)
 main_user.followers.create(follower_id: follower3.id)
 main_user.followers.create(follower_id: follower4.id)
 
-beat1 = main_user.beats.create(name:'Mac Miller Type Beat')
-beat2 = main_user.beats.create(name:'Hard Trap Beat')
+beat1 = main_user.beats.create(name:'Mac Miller Type Beat', description: 'A cool beat I made')
+beat2 = main_user.beats.create(name:'Hard Trap Beat', description: "This beat is my favorite beat I've made so far")
 
 beat1.beat_tags.create(tag: 'chill')
 beat1.beat_tags.create(tag: 'boom bap')
+beat1.beat_tags.create(tag: 'Mac')
+beat1.beat_tags.create(tag: 'Miller')
 
 beat2.beat_tags.create(tag: 'hard')
 beat2.beat_tags.create(tag: 'trap')
+beat2.beat_tags.create(tag: 'Migos')
+beat2.beat_tags.create(tag: 'saucy')
