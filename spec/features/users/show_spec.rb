77c0c874246235f75b102bce7a6480694722b403
Logan Riffell <lkriffell@gmail.com>
2021-03-31 13:23:40 -0400

    Update to reflect changes in the seed file

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -4,19 +4,13 @@ RSpec.describe 'user show' do
   describe 'happy paths' do
     it 'can be shown' do
       user = User.first
-
-      visit login_path
-  
-      fill_in 'email', with: 'BillJ@gmail.com'
-      fill_in 'password', with: '1234'
-    
-      click_button('Log In')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       
       visit "/users/#{user.id}"
 
       expect(page).to have_content("Billy Joel")
       expect(page).to have_content("Followers: 4")
-      expect(page).to have_content("Following: 0")
+      expect(page).to have_content("Following: 1")
       expect(page).to have_content("Billy Joel's Tracks")
       expect(page).to have_content(user.beats.first.name)
       expect(page).to have_content(user.beats.last.name)
