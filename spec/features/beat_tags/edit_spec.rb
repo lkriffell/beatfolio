6e75972f14eee88df45378896090d6ca9d00dd4e
Logan Riffell <lkriffell@gmail.com>
2021-04-06 14:52:32 -0400

    Refactor and add more tests

--- spec/features/beat_tags/edit_spec.rb
+++ spec/features/beat_tags/edit_spec.rb
@@ -2,21 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'beat' do
   before :each do 
-    Beat.first.beat_tags.create(tag:"cool")
-    Beat.first.beat_tags.create(tag:"nice")
-    
-    visit login_path
-  
-    fill_in 'email', with: 'BillJ@gmail.com'
-    fill_in 'password', with: '1234'
-  
-    click_button('Log In')
+    @user = User.first
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    @beat = @user.beats.first
+    @beat.beat_tags.create(tag:"cool")
+    @beat.beat_tags.create(tag:"nice")
+    @beat.beat_tags.create(tag:"dope")
   end
 
   describe 'happy paths' do
     it 'tags can be removed' do
       
-      visit "/beats/#{Beat.first.id}/tags/edit"
+      visit edit_tags_path(@beat)
 
       expect(page).to have_link('cool')
       expect(page).to have_link('dope')
@@ -27,11 +25,13 @@ RSpec.describe 'beat' do
 
       expect(page).to_not have_link('cool')
       expect(page).to_not have_link('nice')
+
+      expect(page).to have_link('dope')
     end
 
     it 'tags can be added' do
       
-      visit "/beats/#{Beat.first.id}/tags/edit"
+      visit edit_tags_path(@beat)
 
       fill_in 'beat_tag_tag', with: 'awesome'
 
@@ -44,13 +44,30 @@ RSpec.describe 'beat' do
   describe 'sad paths' do
     it 'cannot add a blank tag' do
       
-      visit "/beats/#{Beat.first.id}/tags/edit"
+      visit edit_tags_path(@beat)
 
       fill_in 'beat_tag_tag', with: ''
       
       click_button('Add Tag')
 
-      expect(page).to_not have_link('awesome')
+      expect(Beat.first.beat_tags.include?('')).to eq(false)
+    end
+
+    it 'cannot be visited by a logged out user' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(nil)
+
+      visit edit_tags_path(@beat)
+
+      expect(current_path).to eq(login_path)
+    end
+
+    it 'cannot be visited by a user who didnt create the beat' do
+      @user = User.last
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit edit_tags_path(@beat)
+
+      expect(current_path).to eq(beats_show_path(@beat))
     end
   end
 end
