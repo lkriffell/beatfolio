959873048c94c22707ddbb3eac9b19394104caa8
Logan Riffell <lkriffell@gmail.com>
2021-04-27 15:10:02 -0400

    Add test for related_beats model method

--- spec/models/beat_spec.rb
+++ spec/models/beat_spec.rb
@@ -15,4 +15,26 @@ RSpec.describe Beat, type: :model do
     expect(Beat.first).to eq(beat)
     expect(user.beats.first).to eq(beat)
   end
+
+  describe 'model methods' do
+    it 'related_beats finds beats based on tags' do
+      Beat.delete_all
+      user = create :user
+      create :user, username: "miller"
+      beat_one = user.beats.create(name: 'mac miller type beat')
+      beat_two = user.beats.create(name: 'mac miller type beat num two')
+      beat_one.beat_tags.create(tag: "mac")
+      beat_one.beat_tags.create(tag: "miller")
+
+      beat_two.beat_tags.create(tag: "mac")
+
+      beats = Beat.related_beats("mac miller")
+
+      expect(beats["mac"]).to include(beat_one)
+      expect(beats["mac"]).to include(beat_two)
+
+      expect(beats["miller"]).to include(beat_one)
+      expect(beats["miller"]).to_not include(beat_two)
+    end
+  end
 end
