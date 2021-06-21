b76a1ff4a9e5b83bb585ad5e5dc45841fdc5e672
Logan Riffell <lkriffell@gmail.com>
2021-06-21 17:18:42 -0400

    Remove image from user factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user, class: 'User' do
     email { Faker::Internet.email }
     username { Faker::Name.name }
-    image { Faker::Fillmurray.image }
     password { '1234' }
   end
 end
