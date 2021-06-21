acf97d1e846754cc28976b1a50c84e124b53e25a
Logan Riffell <lkriffell@gmail.com>
2021-06-21 17:20:01 -0400

    Fix nokogiri bug

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -10,8 +10,7 @@ class SamplesController < ApplicationController
   def index
     @samples = []
     #goes to search for most downloaded in past 24 hours
-    data = URI.open("https://www.looperman.com/loops?page=1&order=downloads&dir=d&when=1")
-    doc = Nokogiri::HTML(data)
+    doc = Nokogiri::HTML(URI.open("https://www.looperman.com/loops?page=1&order=downloads&dir=d&when=1"))
     doc.css("div#body-content").css("div div.player-wrapper").each do |sample_bundle|
       #instantiates new sample and provides it with scraped attributes
       creator = sample_bundle.css(".player-sub-title").css(".icon-user").text
@@ -23,7 +22,7 @@ class SamplesController < ApplicationController
       # the_sample.creator = sample_creator
       #dives into the sample's url to retrieve "bpm" and "key" tags
       url = sample_bundle.css("div .player-stats-wrapper").css("a").attr("href").text
-      sample_page = Nokogiri::HTML(open(url))
+      sample_page = Nokogiri::HTML(URI.open(url))
       bpm = sample_page.css("div .tag-wrapper a").text.match(/\d\d\d\s(bpm)/).to_s
       key = sample_page.css("div .tag-wrapper a").text.match(/(Key)\s[:]\s\w/).to_s
       the_sample = Sample.new(title, key, download_count, url, audio_url, creator, bpm)
